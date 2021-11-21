9de72490ed31b444cac41a6fa24fca71eed57936
Kitty <zainab_97@hotmail.co.uk>
2021-11-21 19:58:56 +0300

    add some extra controllers and route

--- apis/trips/trips.controllers.js
+++ apis/trips/trips.controllers.js
@@ -8,3 +8,22 @@ exports.tripListFetch = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.fetchTrip = async (tripId, next) => {
+  try {
+    const trip = await Trip.findById(tripId);
+    return trip;
+  } catch (error) {
+    next(error);
+  }
+};
+// fetchTrip not fully tested till I have the create trip - Zainab
+//  Can't finish this till I have the 'User' model -Zainab
+
+// exports.tripCreate = async (req, res, next) => {
+//   try {
+
+//   } catch (error) {
+//     next(error);
+//   }
+// };
--- apis/trips/trips.routers.js
+++ apis/trips/trips.routers.js
@@ -1,8 +1,21 @@
 const express = require("express");
-const { tripListFetch } = require("./trips.controllers");
+const { tripListFetch, fetchTrip } = require("./trips.controllers");
 
+// express
 const router = express.Router();
 
+//Param Middleware
+router.param("tripId", async (req, res, next, tripId) => {
+  const trip = await fetchTrip(tripId, next);
+  if (trip) {
+    req.trip = trip;
+    next();
+  } else {
+    next({ status: 404, message: "Trip Not Found!" });
+  }
+});
+
+// Routers
 router.get("/", tripListFetch);
 
 module.exports = router;
