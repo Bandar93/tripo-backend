a03c114e761fc2e913cd1f8f011d57ef85acdf0f
Kitty <zainab_97@hotmail.co.uk>
2021-11-23 20:02:12 +0300

    fixing and pop

--- apis/trips/trips.controllers.js
+++ apis/trips/trips.controllers.js
@@ -2,7 +2,7 @@ const Trip = require("../../db/model/Trip");
 
 exports.tripListFetch = async (req, res, next) => {
   try {
-    const trips = await Trip.find();
+    const trips = await Trip.find().populate("owner");
     return res.json(trips);
   } catch (error) {
     next(error);
