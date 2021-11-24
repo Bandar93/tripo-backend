ffdd234d636371864434f354e35d4f911b0b24dd
Bandar93 <alshalahi93@outlook.com>
2021-11-24 16:03:55 +0300

    Not finish

--- apis/profile/profiles.controllers.js
+++ apis/profile/profiles.controllers.js
@@ -1,11 +1,25 @@
 const Profile = require("../../db/model/Profile");
 
 exports.ProfileUpdate = async (req, res, next) => {
-    try {
+      const {profileId} = req.params;
+      try {  
       if (req.file) req.body.image = `/${req.file.path}`;
-      await Profile.updateOne(req.body);
+      await Profile.findById(req._id)
+      (req.body);
       res.status(204).end();
+    
     } catch (error) {
       next(error);
     }
-  };
+  }
+
+
+
+  // const { productId } = req.params;
+  // try {   const foundProduct = await Product.findById(productId); 
+  //   if (foundProduct) {     const foundProduct = await Product.findById(productId);     
+  // await foundProduct.updateOne(req.body, { new: true });    
+  //  return res.json(foundProduct);   
+  // } else {  
+  //    return res.status(404).json({ message: "Product not found" });   } } 
+  
